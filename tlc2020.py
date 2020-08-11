615cadae2ce61ebf4d74890dab07fec85f5b4ecc
Yijun Ma <mayijun1203@gmail.com>
2020-08-11 04:31:06 -0400

    Update tlc2020.py

--- tlc2020.py
+++ tlc2020.py
@@ -1,87 +1,80 @@
 import geopandas as gpd
 import pandas as pd
 import numpy as np
-import shapely
-import re
-import datetime
-from shapely import wkt
-from geosupport import Geosupport
-import sqlite3
-import urllib.request
-import shutil
-import os
+import sqlalchemy as sal
 
 
 
 pd.set_option('display.max_columns', None)
 
-path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2020/TLC2020/'
-path='C:/Users/mayij/Desktop/DOC/DCP2020/TLC2020/'
-#path='/home/mayijun/TLC2020/'
-
-
-
-# Download data
-dl=datetime.datetime(2020,4,4)
-for i in range(0,300):
-    dl=dl-datetime.timedelta(days=7)
-    url='http://web.mta.info/developers/data/nyct/turnstile/turnstile_'+datetime.datetime.strftime(dl,'%y%m%d')+'.txt'
-    req=urllib.request.urlopen(url)
-    file = open(path+'DATA/'+datetime.datetime.strftime(dl,'%y%m%d')+'.txt', "wb")
-    shutil.copyfileobj(req,file)
-    file.close()
+path='C:/Users/mayij/Desktop/DOC/DCP2020/TLC2020/FHV/'
+eg=pd.read_csv(path+'engine.csv').loc[0,'engine']
 
 
 
 
+fhvhv='FHVHV202001'
 
-loczone = pd.read_csv(path + 'taxi+_zone_lookup.csv',dtype=object)
-loczone = loczone[['LocationID','Zone']]
-loczone['LocationID'] = pd.to_numeric(loczone['LocationID'], errors='coerce')
-
-bs = pd.read_csv(path + 'baselookup.csv',dtype=object)
-
-
-conn = sqlite3.connect(path + 'fhv2018.sqlite3')
-
-for i in range(1, 13):
-    df = pd.read_csv(path + 'fhv_tripdata_2018-' + str(i).zfill(2) + '.csv', dtype=object)
-    #check the uniqueness of the two duplicate column
-    #np.unique(df['Dispatching_base_num'])
-    
-    df['dispbase'] = np.where(df['Dispatching_base_number'].str.strip().str.upper().isin(['NAN','|',',']),np.nan,df['Dispatching_base_number'].str.strip().str.upper())
-    df = pd.merge(df, bs, how='left', left_on='dispbase', right_on='License Number')
-
-    df['pudatetime'] = pd.to_datetime(df['Pickup_DateTime'].str.strip(), format='%Y-%m-%d %H:%M:%S', errors='coerce')
-    df['pudate'] = df['pudatetime'].dt.date
-    df['pumonth'] = df['pudatetime'].dt.month
-    df['puwkd'] = df['pudatetime'].dt.weekday_name
-    df['puhour'] = df['pudatetime'].dt.hour
-    df['pulocid'] = pd.to_numeric(df['PUlocationID'], errors='coerce')
-    df['dodatetime'] = pd.to_datetime(df['DropOff_datetime'].str.strip(), format='%Y-%m-%d %H:%M:%S', errors='coerce')
-    df['dodate'] = df['dodatetime'].dt.date
-    df['domonth'] = df['dodatetime'].dt.month
-    df['dowkd'] = df['dodatetime'].dt.weekday_name
-    df['dohour'] = df['dodatetime'].dt.hour
-    df['dolocid'] = pd.to_numeric(df['DOlocationID'], errors='coerce')
-    
-    df = pd.merge(df, loczone, how='left', left_on='pulocid', right_on='LocationID')
-    df = pd.merge(df, loczone, how='left', left_on='dolocid', right_on='LocationID')
-    df['dispapp']=df['App Company Affiliation']
-    df['putaxizone']=df['Zone_x']
-    df['dotaxizone']=df['Zone_y']
-    
-    df = df[['dispbase', 'dispapp','SR_Flag', 
-            'pudatetime','pudate','pumonth','puwkd', 'puhour','pulocid','putaxizone', 
-            'dodatetime','dodate','domonth','dowkd', 'dohour','dolocid','dotaxizone']]
-
-    df.to_sql('trip', conn, if_exists='append', index=False)
-    print(i)
-
-conn.close()
+df=pd.read_csv(path+str(fhvhv)+'.csv',dtype=object)
+df['tripid']=[str(fhvhv)+str(x).zfill(10) for x in df.index]
+df['base']=df['dispatching_base_num'].str.strip().str.upper()
+df['app']=np.where(df['hvfhs_license_num']=='HV0003','UBER',
+          np.where(df['hvfhs_license_num']=='HV0004','VIA',
+          np.where(df['hvfhs_license_num']=='HV0005','LYFT','OTHER')))
+df['pudatetime']=pd.to_datetime(df['pickup_datetime'].str.strip(),format='%Y-%m-%d %H:%M:%S',errors='coerce')
+df['pulocid']=pd.to_numeric(df['PULocationID'],errors='coerce')
+df['dodatetime']=pd.to_datetime(df['dropoff_datetime'].str.strip(),format='%Y-%m-%d %H:%M:%S',errors='coerce')
+df['dolocid']=pd.to_numeric(df['DOLocationID'],errors='coerce')
+df['shareflag']=np.where(df['SR_Flag']=='1',1,0)
+df['tripduration']=(df['dodatetime']-df['pudatetime'])/np.timedelta64(1,'s')
+df=df[['tripid','base','app','pudatetime','pulocid','dodatetime','dolocid','shareflag','tripduration']].reset_index(drop=True)
+df.to_csv(path+str(fhvhv)+'-COMPLETE.csv',index=False,header=True,mode='w')
 
 
 
 
 
+
+
+
+# Set up database tables
+engine=sal.create_engine(str(eg))
+con=engine.connect()
+trans=con.begin()
+sql="""
+    CREATE TABLE fhvhv2020
+    (
+      tripid VARCHAR(50),
+      base VARCHAR(10),
+      app VARCHAR(10),
+      pudatetime TIMESTAMP,
+      pulocid REAL,
+      dodatetime TIMESTAMP,
+      dolocid REAL,
+      shareflag REAL,
+      tripduration REAL
+      )
+    """
+con.execute(sql)
+trans.commit()
+con.close()
+# Manually import csv into database
+
+
+
+
+# Manually copy the csv to the cloud
+# Copy the csv to the table
+engine=sal.create_engine(str(eg))
+con=engine.connect()
+trans=con.begin()
+sql="""
+    COPY fhvhv2020
+    FROM '/home/mayijun/TLC2020/fhvhv2020.csv'
+    DELIMITER ','
+    CSV header
+    """
+con.execute(sql)
+trans.commit()
+con.close()
 
